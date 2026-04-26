04ba73169fb72f0269fe6afd5b364f0566e82390
Pham Huu Truong <truongph2@fpt.com>
2026-04-26 14:12:21 +0700

    Fix base URL in vitepress config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Archery Event App Guide",
   description: "User guidelines and documentation for the Archery Event application",
-  base: "/archery-event-bmad/", // Depending on where it will be hosted on GitHub pages, we might need to adjust this. Usually /repo-name/
+  base: "/archery-event-guideline/",
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
